16d1f25c3b2b7c3603bf3da6775fe4f2b5ef0a83
Donald Blanc <dxb3662@rit.edu>
2018-07-06 16:32:07 -0400

    added sneaker card, need to fix state unique ID for state management

--- sneaker_fun/src/App.js
+++ sneaker_fun/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import sneakers from "./sneakers.json";
 import Navpills from "./components/Navpills";
 import Title from "./components/Title";
+import SneakerCard from "./components/SneakerCard";
 import Wrapper from "./components/Wrapper";
 import './App.css';
 
@@ -18,6 +19,43 @@ class App extends Component {
 
   componentDidMount() {
   }
+
+
+  shuffleArray = array => {
+    for (let i = array.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
+
+selectSneaker = name => {
+
+  const findSneaker = this.state.unselected_sneakers.find(item => item.name === name);
+
+  if(findSneaker === undefined){
+      this.setState({
+        banner: " Sorry you guesst incorrectly",
+        highest_score: (this.state.current_score > this.state.highest_score ) ? this.state.current_score : this.state.highest_score,
+        current_score: 0, 
+        sneakers : sneakers,
+        unselected_sneakers: sneakers
+      });
+  }else{
+
+    const newSneaker = this.state.unselected_sneakers.filter(item => item.name !== name);
+
+    this.setState({
+      message: "Huray! you guessed correctly",
+      current_score : this.state.current_score + 1,
+      sneakers : sneakers,
+      unselected_sneakers: newSneaker
+    })
+
+  }
+   this.shuffleArray(sneakers);
+
+}
 
 
   render() {
@@ -38,7 +76,19 @@ class App extends Component {
           highest_score={this.state.highest_score}
         />
         <Title />
+      {
+        this.state.sneakers.map(sneaker =>( 
+          
+          <SneakerCard 
+            name = {sneaker.name}
+            image = {sneaker.image}
+            selectSneaker = {this.selectSneaker}
+            current_score = {this.state.current_score}
+            
+          />
 
+        ))
+      }
 
 
       </Wrapper>
--- sneaker_fun/src/components/SneakerCard/SneakerCard.js
+++ sneaker_fun/src/components/SneakerCard/SneakerCard.js
@@ -1,7 +1,20 @@
 import React from "react";
 import "./SneakerCard.css";
 
-const SneakerCard = props => {
+const SneakerCard = props => (
 
-    
-}
+    <div className="card">
+        <div className="img-container">
+            <a onClick={() => props.selectSneaker(props.name)}
+                className={props.current_score === 0 ? "style_prevu_kit style_prevu_kit_ex" : "style_prevu_kit"}
+            >
+                {console.log(props.name)}
+                {console.log(props.image)}
+                <img alt={props.name} src={props.image} />
+            </a>
+        </div>
+
+    </div>
+);
+
+export default SneakerCard;
